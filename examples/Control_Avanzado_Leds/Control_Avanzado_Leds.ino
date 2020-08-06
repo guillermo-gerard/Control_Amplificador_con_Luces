3dc3f1b39ee88954c22d1c2b1b66dbf19e951faf
facucarroz <facucarroz@gmail.com>
2020-08-06 11:25:27 -0300

    Update

--- examples/Control_Avanzado_Leds/Control_Avanzado_Leds.ino
+++ examples/Control_Avanzado_Leds/Control_Avanzado_Leds.ino
@@ -1,6 +1,9 @@
 #include <Adafruit_NeoPixel.h>
-#include "PixelControl.h" //Libreria que controla los leds ws2812b
 #include <Arduino.h>
+
+#include "AudioControl.h" // Libreria que controla el audio
+#include "Efects.h" //Libreria que controla los efectos
+#include "IEfects.h" //Interfaz de los efectos
 
 byte pinLeds = 2;
 byte pinLecturaAudio = A0;
@@ -9,17 +12,21 @@ int cantidadLeds = 50;
 byte brilloLeds = 255;
 
 Adafruit_NeoPixel pixels(cantidadLeds, pinLeds, NEO_GRB + NEO_KHZ800);// Instancio un objeto pixels
-PixelControl leds(&pixels, cantidadLeds, pinLecturaAudio);//Le paso el objeto pixels
+AudioControl audio(pinLecturaAudio);// Instancio el controlador de audio
+EfectTransition efect_1(&pixels, cantidadLeds, 4, 0.15);// Inizializo el efecto 1
+
+IEfects* efectos[] = {&efect_1};// Creo un array de efectos
 
 long tiempoAnteriorComprobacion = 0;
+int efectoActual = 0;
 
 
 void setup() {
 
     Serial.begin(115200);
 
-    leds.setDetectionSilence(true, 10000, 10);//Activo la deteccion silencio, con un delay entre comprobaciones de 10 segundos y un techo de ruido de 10
-    leds.setDetectionFrequency(1000000, 3, 30);//Leer propiedades.txt
+    audio.setDetectionSilence(true, 10000, 10);//Activo la deteccion silencio, con un delay entre comprobaciones de 10 segundos y un techo de ruido de 10
+    audio.setDetectionFrequency(3750, 5, 30);//Leer propiedades.txt
 
     //Inicializo el objeto pixels
     pixels.begin();
@@ -30,6 +37,6 @@ void setup() {
 
 void loop(){
 
-    Serial.println(leds.readAudio());//Actualizo el estado de los leds
+    efectos[efectoActual]->run(audio.readAudio());//Actualizo el estado de los leds
     
 }
